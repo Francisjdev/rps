947a4006256251cb1335c097e9f05a3535335416
Francis <franciscorojasdev@proton.me>
2025-03-18 11:44:10 -0300

    added som divs to html to be able to play from web pages, change game logic to accommodate input from bottons and display score on webpage. still need to upgrade overall visuals to make it more appealing

--- script.js
+++ script.js
@@ -4,57 +4,77 @@ let userScore = 0;
 let pcScore = 0;
 let keepGoing = true 
 let sentinel = 0
+
+//getting data from webpage to add eventListeners
+const buttons = document.querySelectorAll('button')
+const userResults = document.querySelector('.user-score')
+const pcResults = document.querySelector('.pc-score')
+const winner = document.createElement('div')
+const userScorebox = document.createElement('div')
+const pcScorebox = document.createElement('div')
+
+userResults.textContent = userScore   
+pcResults.textContent = pcScore 
+
+
+
 function getPcChoice(){
     let answer = Math.floor(Math.random()*3);
     if(answer ==0){
-        return pcChoice = 'rock';
+        return 'rock';
     } else if(answer ==1) {
-        return pcChoice = 'paper';
+        return 'paper';
     } else{
-        return pcChoice = 'scissors'
+        return 'scissors'
     }
 }
-function getUserChoice(){
-    return userChoice = prompt('Rock, Paper or Scissors: ').toLowerCase()
+function getClickevent(dombuttons){
+    dombuttons.forEach(button => {
+        button.addEventListener("click", () => {
+            playRound(button.id)
+            displayScore()
+          });
+    });
 }
-function playRound(humanChoice, computerChoice) {
-    return getWinner(humanChoice, computerChoice)
-  }
+
+function playRound(humanChoice){
+    pcChoice = getPcChoice()
+    getWinner(humanChoice, pcChoice)
+    getResult(userScore, pcScore)
+}    
+
 
 function getWinner(humanChoice, computerChoice){
     if (humanChoice === computerChoice){
         console.log("it's a tie ")
         return 
     } else if (humanChoice =='rock' && computerChoice == 'scissors' || humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissors' == computerChoice == 'paper'){
-        console.log('the human wins')
+        console.log(`The Human wins ${humanChoice} beats ${computerChoice}`)
         return ++userScore
     }else {
-        console.log('the machina wins')
+        console.log(`the PC wins ${computerChoice} beats ${humanChoice}` )
       
         return ++pcScore
     } 
 }
-function playGame(humanChoice, computerChoice){
-    return playRound(humanChoice, computerChoice)
-}
+
 function getResult(userResult, pcResult){
-    if (userResult > pcResult){
-        console.log(`The Human wins ${userResult} to ${pcResult}`)
-    } else if (userResult === pcResult){
-        console.log("It's a Tie")
-    } else {
-       console.log(`the PC wins ${pcResul} to ${userResult}` )
+    if(userResult === 5 || pcResult === 5){
+        if (userResult > pcResult){
+            console.log(`The Human wins ${userResult} to ${pcResult}`)
+        } else if (userResult === pcResult){
+            console.log(`It's a Tie. The score is ${pcResult} to ${userResult}`)
+        } else {
+           console.log(`the PC wins ${pcResult} to ${userResult}` )
+        }
+        userScore = 0
+        pcScore = 0 
     }
+    }
+   
+function displayScore(){
+    userResults.textContent = userScore   
+    pcResults.textContent = pcScore 
 }
 
-
-while(keepGoing){
-    const humanSelection = getUserChoice();
-    const computerSelection = getPcChoice();
-    playGame(humanSelection,computerSelection)
-    sentinel++
-    console.log(sentinel)
-    if(sentinel == 5) keepGoing = false;
-}
-getResult(userScore, pcScore)
-
+getClickevent(buttons)
